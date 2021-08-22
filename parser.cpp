3b969f75e003e375d5735427d4e0d8fcd7260708
JailSeed <45213700+JailSeed@users.noreply.github.com>
2021-08-22 18:42:10 +0300

    Added while(true) to loop the input for new CIDs

--- parser.cpp
+++ parser.cpp
@@ -4,10 +4,12 @@ int main()
 {
     unsigned int LCID, CID;
     unsigned short sector;
-    std::cout << "Enter LCID: ";
-    std::cin >> LCID;
-    CID = LCID % 65536;
-    sector = CID % 10;
-    std::cout << "CID: " << CID / 10 << std::endl << "Sector: " << sector;
-    return 0;
+    while (true) {
+        std::cout << "Enter LCID: ";
+        std::cin >> LCID;
+        CID = LCID % 65536;
+        sector = CID % 10;
+        std::cout << "CID: " << CID / 10 << std::endl << "Sector: " << sector << std::endl;
+        std::cout << "---------------------" << std::endl;
+    }
 }
